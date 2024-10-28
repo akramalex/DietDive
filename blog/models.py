4ac8a98dbb88c96c6d981c6645a5dd4bfcd95469
Akram Ewida Ahmed Ewida <info@quick4you.net>
2024-10-28 15:41:35 +0000

    add class meta for both like and comment

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,12 @@ from django.contrib.auth.models import User
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
+
+LIKE_CHOICES =(
+    ('Like', 'Like'),
+    ('Unlike', 'Unlike')
+)
+
 # creat your models here.
 
 class Post(models.Model):
@@ -15,3 +21,50 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
+
+
+
+class Like(models.Model):
+    """
+    Stores a single like entry related to :model:`auth.User`
+    and :model:`blog.Post`.
+    """
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='likes'
+    )
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='likes'
+    )
+    value = models.CharField(
+        choices=LIKE_CHOICES, default='Like', max_length=10
+    )
+
+    class Meta:
+        unique_together = ('user', 'post')  # Ensure a user can only like a post once
+
+    def __str__(self):
+        return f"{self.user} {'likes' if self.value == 'Like' else 'dislikes'} {self.post.title}"
--- dietdive/settings.py
+++ dietdive/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-akramalex-dietdive-l8xkm7lwic6.ws.codeinstitute-ide.net','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-akramalex-dietdive-9xwmeslqna3.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
