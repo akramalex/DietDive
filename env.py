503028737b6fe669fbae9b194a51a90ddea718f3
Akram Ewida Ahmed Ewida <info@quick4you.net>
2024-10-28 11:26:47 +0000

    fix a bug error steps

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ.setdefault(
-    "DATABASE_URL", "postgres://u9jofjis4rg:6S4Lp7xzBZhG@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/jet_fifty_audio_643668")
-os.environ.setdefault("SECRET_KEY", "akram")
