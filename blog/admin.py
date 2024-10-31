5a6ec47417eccec66dca8136933f92e2e36b6a7c
Akram Ewida Ahmed Ewida <info@quick4you.net>
2024-10-31 14:06:47 +0000

    uploaded data which going to edit later

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Post, Comment,Like
 
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
+
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Like)
