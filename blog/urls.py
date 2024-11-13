61ba66158ffa7308264c469f7781bf37d195e825
Akram Ewida Ahmed Ewida <info@quick4you.net>
2024-11-13 18:00:02 +0000

    fixing bug post

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('post/<slug:slug>/', views.post_detail, name='post_detail'),
     path('post/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
