71c0a18e1494b866ae527c1e224772e399ffc1a3
Akram Ewida Ahmed Ewida <info@quick4you.net>
2024-11-25 14:22:54 +0000

    added message if contact form invaild

--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import ContactForm
 
 # Create your views here.
@@ -8,6 +9,10 @@ def contact_view(request):
         if form.is_valid():
             form.save()
             return redirect('contact_success')  # Redirect to the success view
+        else:
+            # If form is invalid, send an error message
+            messages.error(request, "The form is invalid. Please try again.")
+            return render(request, 'contact/contact.html', {'form': form})  # Render the form again with error    
     else:
         form = ContactForm()
     return render(request, 'contact/contact.html', {'form': form})
